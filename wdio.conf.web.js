51d59f907a3956bfbe0e4a3b474ea7418ef23265
Neerajkumar <neerajlad171@gmail.com>
2023-09-17 08:17:48 +0100

    yaml and browserstack integration

--- wdio.conf.web.js
+++ wdio.conf.web.js
@@ -1,4 +1,5 @@
 var path = require("path");
+const { TimelineService } = require("wdio-timeline-reporter/timeline-service");
 exports.config = {
   //
   // ====================
@@ -129,7 +130,7 @@ exports.config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: [["chromedriver"]],
+  services: [["chromedriver"], [TimelineService]],
 
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
@@ -151,19 +152,20 @@ exports.config = {
   // Test reporter for stdout.
   // The only one supported by default is 'dot'
   // see also: https://webdriver.io/docs/dot-reporter
-  reporters: [
-    "spec",
-    [
-      "junit",
-      {
-        outputDir: "./reports",
-        outputFileFormat: function (options) {
-          // optional
-          return `results-${options.cid}.${options.capabilities}.xml`;
-        },
-      },
-    ],
-  ],
+  // reporters: [
+  //   "spec",
+  //   [
+  //     "junit",
+  //     {
+  //       outputDir: "./reports",
+  //       outputFileFormat: function (options) {
+  //         // optional
+  //         return `junit-results.xml`;
+  //       },
+  //     },
+  //   ],
+  // ],
+  reporters: [["timeline", { outputDir: "./reports" }]],
 
   //
   // If you are using Cucumber you need to specify the location of your step definitions.
